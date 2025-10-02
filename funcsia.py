0ee96975c5f50f670ef3e6a09391962bb3ccb43e
alkrrl <alenakarabunarly@yandex.ru>
2025-10-02 08:29:59 +0300

    Create funcsia.py

---
+++ funcsia.py
@@ -0,0 +1,16 @@
+def Abs(x):
+    result = int((x**2)**0.5)
+    return result
+
+
+def Square(x): 
+    return x**2
+
+def Sqrt(x):
+    return x**0.5
+
+def Gip(x):
+    return 1/x
+
+def Prum(x):
+    return 2*x
